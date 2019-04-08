7b6ef3b735e96ac5016c5c1ae773c989e0ad33f2
Benjamin Kroeger <benjamin.kroeger@gmail.com>
2019-04-08 22:34:36 +0200

    updates dependencies

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // node core modules
 
 // 3rd party modules
@@ -8,10 +6,22 @@ import test from 'ava';
 // internal modules
 const utils = require('../lib/utils');
 
-test((t) => {
+test(t => {
   t.is(typeof utils.parse, 'function', 'utils.parse should be a function');
   t.is(typeof utils.select, 'function', 'utils.select should be a function');
-  t.is(typeof utils.serialize, 'function', 'utils.serialize should be a function');
-  t.is(typeof utils.selectUseNamespaces, 'function', 'utils.selectUseNamespaces should be a function');
-  t.is(typeof utils.ensureXMLDoc, 'function', 'utils.ensureXMLDoc should be a function');
+  t.is(
+    typeof utils.serialize,
+    'function',
+    'utils.serialize should be a function',
+  );
+  t.is(
+    typeof utils.selectUseNamespaces,
+    'function',
+    'utils.selectUseNamespaces should be a function',
+  );
+  t.is(
+    typeof utils.ensureXMLDoc,
+    'function',
+    'utils.ensureXMLDoc should be a function',
+  );
 });
